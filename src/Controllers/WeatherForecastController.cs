e0544dee025f19f69102c606d658f26cbd5d6a96
marufoi22 <m.y.882soccer@gmail.com>
2025-03-20 22:18:52 +0900

    edit API

--- src/Controllers/WeatherForecastController.cs
+++ src/Controllers/WeatherForecastController.cs
@@ -24,18 +24,20 @@ namespace BuildCalc.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IActionResult Get()
         {
             _logger.LogInformation("Weather forecast requested");
             
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            var forecasts =  Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 35),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+
+            return Ok(forecasts);
         }
     }
 
